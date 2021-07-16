efa319b668e0dacbb669a3636cea575280138662
Palakshi-Das <78961040+Palakshi-Das@users.noreply.github.com>
2021-07-16 20:51:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ block_image_height = 430;
 
 var block_image_object= "";
 
-function new_image(get_image)
+function new_Image_update(get_image)
 {
-	fabric.Image.fromURL(get_img , function(Img){
+	fabric.Image.fromURL(get_image , function(Img){
         block_image_object = Img;
 
         block_image_object.scaleToWidth(block_image_width);
         block_image_object.scaleToHeight(block_image_height);
         block_image_object.set({
-            top:player_y , 
-            left : player_x 
+            top:block_y , 
+            left : block_x 
         });
         canvas.add(block_image_object);
     });
